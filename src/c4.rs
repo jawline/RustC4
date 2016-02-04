ed060d25470ee72efb9d3c2a8987ad21b9cabe05
Blake Loring <blake_l@parsed.uk>
2016-02-04 17:27:28 +0000

    Random insert approach works

--- src/c4.rs
+++ src/c4.rs
@@ -5,14 +5,27 @@ pub struct C4 {
 }
 
 impl C4 {
+
 	pub fn new() -> C4 {
 		C4 {
 			board: Board::new()
 		}
 	}
 
+	fn find_row_for_insert(&self, col: usize) -> Option<usize> {
+		(0..self.board.height()).find(|&row| !self.board.get(col, row))
+	}
+
+	pub fn insertable_columns(&self) -> Vec<usize> {
+		(0..self.board.width()).filter(|&col| self.can_insert(col)).collect()
+	}
+
+	pub fn can_insert(&self, col: usize) -> bool {
+		return self.find_row_for_insert(col).is_some()
+	}
+
 	pub fn insert(&mut self, col: usize) -> bool {
-		let row = (0..self.board.height()).find(|&row| !self.board.get(col, row));
+		let row = self.find_row_for_insert(col);
 
 		if row.is_some() {
 			self.board.set(true, col, row.unwrap());
@@ -20,6 +33,10 @@ impl C4 {
 		} else {
 			false
 		}
+	}
+
+	pub fn is_won() -> bool {
+		false
 	}
 
 	pub fn print(&self) {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
+extern crate rand;
+
 mod board;
 mod c4;
 
+use rand::*;
+
+fn print_round(b1: &c4::C4, round: usize) {
+	println!("Round: {}", round);
+	b1.print();
+}
+
+fn random_insert(b1: &mut c4::C4, rng: &mut rand::ThreadRng) {
+	let insertable = b1.insertable_columns();
+
+	let pick = if insertable.len() == 1 {
+		0
+	} else {
+		rng.gen::<usize>() % insertable.len()
+	};
+
+	b1.insert(insertable[pick]);
+	println!("Picked {} of {}", pick, insertable.len());
+}
+
 fn main() {
     let mut b1 = c4::C4::new();
-    b1.insert(3);
-    b1.insert(3);
-    b1.insert(1);
-    b1.insert(0);
+    let mut rng = rand::thread_rng();
+
+    let mut round = 0;
+
     b1.print();
+    print_round(&b1, round);
+
+    while b1.insertable_columns().len() > 0 {
+    	random_insert(&mut b1, &mut rng);
+    	round = round + 1;
+    	print_round(&b1, round);
+    }
 }
